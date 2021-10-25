05ac940b60b201298fe2c5eae2370b53d4cff80a
Rodrigo <rodrigocosta47@outlook.br>
2021-10-25 18:02:05 -0300

    Criei as funcoes que fazem as transicoes

--- files/js/main.js
+++ files/js/main.js
@@ -1,2 +1,31 @@
-const beforeBtn = document.getElementsByClassName('js-before-btn'),
-afterBtn = document.getElementsByClassName('js-after-btn');
+var nBtn = document.querySelector('.js-after-btn'),
+pBtn = document.querySelector('.js-before-btn'),
+widthMasc = document.querySelector('.carrocel-itens').clientWidth,
+slider = document.querySelector('.carrocel-slider'),
+i = 0;
+
+
+
+var nextBtn = function(){
+    nBtn.addEventListener('click', function() {
+        i++
+        console.log(i)
+        slider.style.transition = '.5s'
+        slider.style.transform = `translate(${(-widthMasc) * i}px)`
+        // if (i >= slider.childElementCount){
+        //     i = 0
+        // }
+    });
+};
+
+var prevtBtn = function(){
+    pBtn.addEventListener('click', function() {
+        i--
+        console.log(i)
+        slider.style.transition = '.5s';
+        slider.style.transform = `translate(${(-widthMasc) * i}px)`;
+    });
+};
+
+nextBtn();
+prevtBtn();
